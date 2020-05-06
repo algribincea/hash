a2fef2d41059d9cf06337b37e650262b8d491f17
alina <gribinceaalina34@gmail.com>
2020-05-06 23:36:34 +0300

    hash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hash';
+
+  value = '';
+  rezult = '';
+  text = "";
+  rezult2
+
+
+  criptValue () {
+    this.rezult=''
+    let str = new String(this.value);
+
+    for (let i of str) {
+      console.log(i);
+
+      this.rezult += (this.hash()+ (i.charCodeAt(0) & 255) +this.hash());
+    }
+
+    str = '';
+    this.method2()
+  }
+
+  hash() {
+    this.text= ''
+    let possible = "01002030405ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_*";
+    for (let j = 0; j < 10; j++) {
+      this.text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return this.text
+  }
+
+  method2() {
+    var a = 1, c = 0, h, o;
+    if (this.value) {
+        a = 0;
+        /*jshint plusplus:false bitwise:false*/
+        for (h = this.value.length - 1; h >= 0; h--) {
+            o = this.value.charCodeAt(h);
+            a = (a<<6&268435455) + o + (o<<14);
+            c = a & 266338304;
+            a = c!==0?a^c>>21:a;
+        }
+    }
+    this.rezult2 = a
+  }
+
+
+
+
+
 }
